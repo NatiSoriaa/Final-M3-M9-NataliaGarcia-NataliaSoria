f92522a9afdd1b549ff01ebd93a1eef77b00196d
NatiSoriaa <naticsoria@gmail.com>
2025-05-07 18:01:50 +0200

    actualizacion login register

--- MP9-3_PR_NataliaSoria-Garcia/LibraryApi/public/js/index.js
+++ MP9-3_PR_NataliaSoria-Garcia/LibraryApi/public/js/index.js
@@ -41,18 +41,75 @@
 // DROPDOWN MENU USER 
 
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const userIcon = document.querySelector(".user-icon");
+//   const dropdownMenu = document.querySelector(".dropdown-menu");
+
+//   userIcon.addEventListener("click", (event) => {
+//     event.preventDefault(); 
+//     dropdownMenu.classList.toggle("show"); 
+//   });
+
+//   document.addEventListener("click", (event) => {
+//     if (!userIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+//       dropdownMenu.classList.remove("show");
+//     }
+//   });
+// });
+
+
+
+
+// LOGIN Y REGISTER
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
-  const userIcon = document.querySelector(".user-icon");
-  const dropdownMenu = document.querySelector(".dropdown-menu");
+  setupFormEvents();
+  setupModalEvents();
 
-  userIcon.addEventListener("click", (event) => {
-    event.preventDefault(); 
-    dropdownMenu.classList.toggle("show"); 
-  });
+  function setupFormEvents() {
 
-  document.addEventListener("click", (event) => {
-    if (!userIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
-      dropdownMenu.classList.remove("show");
-    }
-  });
+      document.querySelector('#loginForm').addEventListener('submit', (event) => {
+          event.preventDefault();
+          const email = document.getElementById('log-user').value;
+          const password = document.getElementById('log-passw').value;
+          localStorage.setItem('loggedEmail', email); 
+          login(email, password); 
+      });
+
+      document.querySelector('#registerForm').addEventListener('submit', (event) => {
+          event.preventDefault();
+          const username = document.getElementById('reg-user').value;
+          const email = document.getElementById('reg-email').value;
+          const password = document.getElementById('reg-passw').value;
+          createUser(username, email, password); 
+      });
+
+  }
+
+  function setupModalEvents() {
+      const loginBtn = document.getElementById("showLogin");
+      const registerBtn = document.getElementById("showRegister");
+      const loginForm = document.getElementById("loginForm");
+      const registerForm = document.getElementById("registerForm");
+
+      loginForm.classList.add("active");
+
+      loginBtn.addEventListener("click", () => {
+          toggleActiveState(loginForm, registerForm, loginBtn, registerBtn);
+      });
+
+      registerBtn.addEventListener("click", () => {
+          toggleActiveState(registerForm, loginForm, registerBtn, loginBtn);
+      });
+
+      function toggleActiveState(activeForm, inactiveForm, activeBtn, inactiveBtn) {
+          activeForm.classList.add("active");
+          inactiveForm.classList.remove("active");
+          activeBtn.classList.add("active");
+          inactiveBtn.classList.remove("active");
+      }
+  }
+  
 });
